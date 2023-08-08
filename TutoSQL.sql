5c75c3e1623b17d2528463bdd12fe2b840fca97a
ThierrydelCastillo <dev@del-castillo.fr>
2023-08-08 18:08:40 +0200

    05 - Clés étrangères et jointures

--- TutoSQL.sql
+++ TutoSQL.sql
@@ -1,42 +1,47 @@
 -- MYSQL
 
--- EXERCICE
 DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS categories;
 
--- CREATION TABLE recettes
-CREATE TABLE recipes (
+CREATE TABLE IF NOT EXISTS categories (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(150) NOT NULL,
-    slug VARCHAR(50)NOT NULL UNIQUE,
-    content TEXT,
-    duration SMALLINT DEFAULT 10 NOT NULL,
-    online BOOLEAN,
-    created_at TIMESTAMP  
+    description TEXT
 ) ENGINE=InnoDB;
 
--- INDEXER CERTAINS CHAMPS
--- SHOW INDEX FROM recipes;
--- CREATE UNIQUE INDEX idx_slug ON recipes (slug);
--- DROP INDEX idx_slug ON recipes;
+CREATE TABLE recipes (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    slug VARCHAR(50) NOT NULL UNIQUE,
+    content TEXT,
+    category_id INTEGER,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+        ON DELETE SET NULL
+) ENGINE=InnoDB;
 
--- UPDATE recipes SET slug = 'soupe-3' WHERE id = 3;
+-- ALTER TABLE recipes 
+--     ADD FOREIGN KEY (category_id) REFERENCES categories(id)
+--     ON DELETE SET NULL;
 
-SET time_zone='+00:00'; -- Nécéssaire pour insérer une date.
-INSERT INTO recipes (
-        title,
-        slug,
-        online,
-        created_at
-    ) VALUES (
-        'Soupe1',
-        'soupe',
-        FALSE,
-        NULL
-    );
+INSERT INTO categories (title) 
+VALUES ('Plat'), ('Dessert');
 
-SELECT * FROM recipes WHERE content IS NULL;
+INSERT INTO recipes (title, slug, category_id) 
+VALUES 
+    ('Crème anglaise','creme-anglaise',2),
+    ('Soupe', 'soupe', 1),
+    ('Salade de fruits', 'salade-de-fruits', NULL);
+
+-- DELETE FROM categories WHERE id = 2;
+
+SELECT r.id, r.title, slug, content, c.title AS category 
+FROM recipes r
+-- LEFT JOIN categories c ON category_id = c.id; /* AVEC les champs avec category_id a NULL */
+JOIN categories c ON category_id = c.id /* SANS les champs avec category_id a NULL */
+WHERE c.title = 'Dessert';
+
+
 
 -- DELETE FROM recipes WHERE id = 2;
-
 -- UPDATE recipes SET content = NULL;
 -- UPDATE recipes SET title = 'Soupe de légumes' WHERE title = 'soupe '; 
